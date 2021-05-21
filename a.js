c3800543c618be58dbf7b24fd858175f39577848
Connor Ahern <dck.dachickenking@gmail.com>
2021-05-21 11:16:58 -0700

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,20 @@
-// gimme a moment
+// © Connor Ahern 2021
+// Licensed under MPL 2.0
+
+var SlackImprover = {
+  "alreadyInit": false,
+  "init": function(force) {
+    if(this.alreadyInit == true && !(force)) {
+      return console.error("SlackImprover Error - Already initalized.");
+    } else if (this.alreadyInit == true && force) {
+      console.warn("SlackImprover is initializing again, even though it already has this session.");
+    }
+    
+    $(".p-top_nav__sidebar").innerHTML = "SlackImprover Alpha V1.0 " + $(".p-top_nav__sidebar").innerHTML;
+    
+    this.alreadyInit = true;
+  }
+}
+
+// Start everything!
+SlackImprover.init(false);
